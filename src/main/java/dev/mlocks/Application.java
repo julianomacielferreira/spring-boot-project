f574de47bb58d5469dc3e65b6ef6565c59f9394f
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-07-06 18:23:03 -0300

    Update gradle configs to use java 22.

--- src/main/java/dev/mlocks/Application.java
+++ src/main/java/dev/mlocks/Application.java
@@ -27,10 +27,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostsApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostsApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
