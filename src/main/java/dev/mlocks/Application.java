6a9c1523a5cb4f0446948450c67cad4d55113dd8
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-07-06 18:08:45 -0300

    Rename application bootstrap file.

--- src/main/java/dev/mlocks/Application.java
+++ src/main/java/dev/mlocks/Application.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package dev.mlocks.posts;
+package dev.mlocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
