c3c7a7c9520ce85fa0f742bf1866cfbe5ead862e
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-07-30 21:50:52 -0300

    Create get comments endpoint test.

---
+++ src/test/java/dev/mlocks/comments/CommentsControllerTest.java
@@ -0,0 +1,93 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 juliano.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.mlocks.comments;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(Comment.class)
+@AutoConfigureMockMvc
+public class CommentsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    List<Comment> comments = new ArrayList();
+
+    @MockitoBean
+    CommentRepository commentRepository;
+
+    @BeforeEach
+    void setUp() {
+        // create some comments
+        this.comments = List.of(
+                new Comment(1, 1, "Name 1", "email1@email.com", "This is my first comment", null),
+                new Comment(2, 2, "Name 2", "email2@email.com", "This is my second comment", null)
+        );
+    }
+
+    @Test
+    void shouldFindAllComments() throws Exception {
+
+        String jsonResponse = """
+                [
+                    {
+                      "postId": 1,
+                      "id": 1,
+                      "name": "Name 1",
+                      "email": "email1@email.com",
+                      "body": "This is my first comment,
+                      "version": null
+                    },
+                    {
+                      "postId": 2,
+                      "id": 2,
+                      "name": "Name 2",
+                      "email": "email2@email.com",
+                      "body": "This is my second comment,
+                      "version": null
+                    }
+                ]
+                """;
+
+        when(this.commentRepository.findAll()).thenReturn(this.comments);
+
+        this.mockMvc.perform(get("/api/comments")).
+                andExpect(status().isOk()).
+                andExpect(content().json(jsonResponse));
+    }
+}
