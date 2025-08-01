d5c3e770138c237581d2f555da43a7769e990470
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-01 20:07:39 -0300

    Annotate exception for comment not found.

--- src/main/java/dev/mlocks/comments/CommentNotFoundException.java
+++ src/main/java/dev/mlocks/comments/CommentNotFoundException.java
@@ -23,6 +23,10 @@
  */
 package dev.mlocks.comments;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CommentNotFoundException extends RuntimeException {
     public CommentNotFoundException(String message) {
         super(message);
