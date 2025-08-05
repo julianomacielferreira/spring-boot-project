e1d66b28564d6cd3feb5a4686c2c0bb52f8c7759
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-04 21:29:04 -0300

    Create an AbstractController to remove duplicated logic.

---
+++ src/main/java/dev/mlocks/util/AbstractController.java
@@ -0,0 +1,92 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 juliano.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.mlocks.util;
+
+import jakarta.validation.Valid;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class AbstractController<T, ID, R extends ListCrudRepository<T, ID>, E extends Exception> {
+
+    protected final R repository;
+    protected final Class<E> exceptionClass;
+
+    public AbstractController(R repository, Class<E> exceptionClass) {
+        this.repository = repository;
+        this.exceptionClass = exceptionClass;
+    }
+
+    @GetMapping("")
+    List<T> findAll() {
+        return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<T> findById(@PathVariable ID id) throws Exception {
+        return Optional.of(this.repository.findById(id).orElseThrow(() -> {
+            try {
+                return exceptionClass.getConstructor(String.class).newInstance("Entity not found");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    T create(@RequestBody @Valid T entity) {
+        return this.repository.save(entity);
+    }
+
+    @PutMapping("/{id}")
+    T update(@PathVariable ID id, @RequestBody @Valid T entity) {
+
+        Optional<T> existing = this.repository.findById(id);
+
+        if (existing.isPresent()) {
+            return this.repository.save(entity);
+        } else {
+            throwException("Entity not found");
+            return null; // never reach this point
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable ID id) {
+        this.repository.deleteById(id);
+    }
+
+    protected void throwException(String message) {
+        try {
+            throw exceptionClass.getConstructor(String.class).newInstance(message);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
