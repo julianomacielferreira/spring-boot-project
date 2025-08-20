3592ca9c406840445b08787ec3d9a5b0e4b933c9
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-20 13:51:32 -0300

    Add GeoLocation entity and related structures.

--- src/main/java/dev/mlocks/address/Geo.java
+++ src/main/java/dev/mlocks/address/Geo.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package dev.mlocks.users;
+package dev.mlocks.address;
 
 import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.annotation.Id;
